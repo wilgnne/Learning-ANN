3eafc2edf5d4491464534db3bdfedc4998b721c6
Wilgnne <w.khawanbarbosa@gmail.com>
2019-10-15 22:58:57 -0300

    Cupy implementation

--- __init__.py
+++ __init__.py
@@ -1,4 +1,12 @@
-import numpy as np
+try:
+    import cupy as np
+    np.array([0])
+    print("GPU")
+except:
+    import numpy as np
+    print("CPU")
+import matplotlib.pyplot as plt
+import time
 
 class Brain (object):
     '''Brain Class: Representação da Rede Neural
@@ -15,10 +23,11 @@ class Brain (object):
         #Expresao lambida que seleciona o método a ser usado para gerar os pesos
         funcGenereate = (lambda x: np.ones(x)) if ones else (lambda x: np.random.random(x))
 
+
         #Matriz de pesos da rede
         self.pesos = []
         for i in range(len(self.architecture)-1):
-            weight = funcGenereate((self.architecture[i],  self.architecture[i + 1]))
+            weight = funcGenereate((self.architecture[i + 1], self.architecture[i]))
             self.pesos.append( 2 * weight - 1)
 
     def think (self, inputs: np.array):
@@ -26,8 +35,8 @@ class Brain (object):
         #Propagação das sinapses por dentro da rede neural
         sinapses = inputs
         for hidden in self.pesos:
-            weights = np.dot(sinapses, hidden)
-            sinapses = weights * Brain.sigmoid(weights)
+            weights = np.dot( hidden, sinapses)
+            sinapses = Brain.sigmoid(weights)
         
         return sinapses
 
@@ -36,6 +45,38 @@ class Brain (object):
         '''Função Sigmoidal de ativação dos neuronios'''
         return 1 / (1 + np.exp(-x))
 
+from mpl_toolkits.mplot3d import axes3d
 if __name__ == "__main__":
-    a = Brain(2, [20], 1, ones=False)
-    print(a.think(np.array([10, 20])))
+    a = Brain(2, [100]*10, 1, ones=False)
+
+    # Grab some test data.
+    X, Y, Z = axes3d.get_test_data(0.05)
+    full = 0
+    z = []
+    for i in range(120):
+        for j in range(120):
+            t = time.time()
+            entry = np.array([X[i][j], Y[i][j]]).T
+            out = a.think(entry)
+            z.append(float(out))
+            full += time.time() - t
+    
+    print("{:.10f}".format(full/(120*120)))
+
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    Z = np.array(z)
+    Z.shape = (120, 120)
+
+    try:
+        Z = np.asnumpy(Z)
+    except:
+        pass
+
+
+    # Plot a basic wireframe.
+    ax.plot_surface(X, Y, Z, cmap='viridis')
+
+    plt.show()
