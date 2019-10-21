57c15f9a314d5f28b7a8d4ff846c078fc61fc61a
Wilgnne <w.khawanbarbosa@gmail.com>
2019-10-21 16:56:26 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 echo "Install Neural Network - Wilgnne K."
+sudo rm -r /usr/lib/python3.7/NeuralNetwork;
 sudo rm -r /usr/lib/python3.7/site-packages/NeuralNetwork;
-sudo git clone https://github.com/Wilgnne/Learning-ANN.git /usr/lib/python3.7/site-packages/NeuralNetwork
+
+sudo git clone https://github.com/Wilgnne/Learning-ANN.git /usr/lib/python3.7/NeuralNetwork
